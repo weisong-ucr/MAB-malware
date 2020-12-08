c0a6c4f7d8320873334b1ba011a8d105239e0685
weisong <wsong008@ucr.edu>
2020-12-07 22:15:21 -0800

    before run on Azure

---
+++ chart/evasion_rate_MCTS.py
@@ -0,0 +1,63 @@
+import os
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+def get_from_log(log, r):
+    list_rate = []
+    with open(log, 'r') as fp:
+        for line in fp:
+            if 'RESULT' in line:
+                line = line.strip()
+                print(line.split()[7], line.split()[9])
+                total = int(line.split()[5])
+                succ = int(line.split()[7][:-1])
+                fail = int(line.split()[9])
+                print(succ,fail)
+                rate = succ / total
+                list_rate.append(rate)
+    return list_rate + [rate]*(total-len(list_rate)), total
+
+def main():
+    av = 'malconv'
+    r1 = 1
+    r2 = 1
+    r3 = 1
+
+    #y1_list_rate, _ = get_from_log('/home/wei/code/adversarial_malware/final_output/our/output_' + av + '_1000_random/rewriter.log', r1)
+    ##y2_list_rate, _ = get_from_log('/home/wei/code/adversarial_malware/final_output/our/output_' + av + '_1000_TS/rewriter.log', r2)
+    #y3_list_rate, total = get_from_log('/home/wei/code/adversarial_malware/final_output/our/output_' + av + '_1000_TS_parent/rewriter.log', r3)
+
+    y1_list_rate, _ = get_from_log('/home/wei/code/MAB-malware/MCTS.rewriter.log', r1)
+    #y2_list_rate, _ = get_from_log('/home/wei/code/adversarial_malware/final_output/our/malconv/output_' + av + '_holdout_TS/rewriter.log', r2)#
+    y3_list_rate, total = get_from_log('/home/wei/code/MAB-malware/MCTS.rewriter2.log', r3)
+
+    x1 = np.array(range(len(y1_list_rate)))
+    #x2 = np.array(range(len(y2_list_rate)))
+    x3 = np.array(range(len(y3_list_rate)))
+    
+    fig, ax = plt.subplots()
+    ax.plot(x1, y1_list_rate, fillstyle='none', linewidth=1, color='blue', label='Random')
+    #ax.plot(x2, y2_list_rate, fillstyle='none', linewidth=1, color='red', label='TS')
+    ax.plot(x3, y3_list_rate, fillstyle='none', linewidth=1, color='red', label='TS')
+    axes = plt.gca()
+    #axes.set_ylim([0,60])
+    axes.set_xlim([0, total * 1.1])
+    
+    plt.annotate(str(y1_list_rate[-1]), (total,y1_list_rate[-1]), color='blue', fontsize=9)
+    #plt.annotate(str(y2_list_rate[-1]), (total,y2_list_rate[-1]-3), color='red', textcoords="offset points", fontsize=9)
+    plt.annotate(str(y3_list_rate[-1]), (total,y3_list_rate[-1]-3), color='red', fontsize=9)
+
+    plt.xlabel('total number of attempts', fontsize=12)
+    plt.ylabel('evasion rate %', fontsize=12)
+    ax.legend(loc='lower right', ncol=2)#, framealpha=0)
+    fig.subplots_adjust(bottom=0.5)
+    
+    #plt.show()
+    #matplotlib.pyplot.title(av);
+    plt.savefig("evasion_rate_of_attempts_%s_our.pdf" %av)
+
+if __name__ == '__main__':
+    main()
+
